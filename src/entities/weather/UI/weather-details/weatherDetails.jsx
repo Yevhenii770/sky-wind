5e02c27cece927202a7d4b37b2e46d029fe1d269
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-08-18 11:42:19 +0200

    added max/min temperature

--- src/entities/weather/UI/weather-details/weatherDetails.jsx
+++ src/entities/weather/UI/weather-details/weatherDetails.jsx
@@ -1,26 +1,47 @@
+import { useSelector } from 'react-redux';
+import { selectAllWeather } from '../../../../redux/weather/weather-selectors';
 import { AIcon } from '../../../../shared/components/UI/atoms';
 import { MSliderDay } from '../../../../shared/components/UI/molecules/MSliderDay';
 import './styles.scss';
 
 export const WeatherDetails = () => {
+  const weatherArray = useSelector(selectAllWeather);
+  const currentWeather = weatherArray.current;
+
+  let temperatureArr = [];
+  weatherArray.hourly.data.map((el) =>
+    temperatureArr.push(Math.round(el.temperature))
+  );
+
   return (
     <>
       <div className="weather-day__now">
         <div className="weather-day__container-icon">
-          <AIcon name="cloudy" size="120" />
+          <AIcon name={currentWeather.summary} size="120" />
         </div>
         <div>
-          <div className="weather-day__big-celsius">12&#176;</div>
-          <div>Cloudy, 16&#176; / 5&#176;</div>
+          <div className="weather-day__big-celsius">
+            {Math.round(currentWeather.temperature)}&#xb0;
+          </div>
+          <div>
+            {weatherArray.current.summary}, {Math.max(...temperatureArr)}&#176;
+            / {Math.min(...temperatureArr)}&#176;
+          </div>
         </div>
       </div>
       <div className="detail-info">
         <div className="detail-info__box">
           <div className="detail-info__icon-box">
-            <AIcon name="rain" size="20" className="detail-info__box-icon" />
+            <AIcon
+              name={currentWeather.summary}
+              size="20"
+              className="detail-info__box-icon"
+            />
           </div>
-          <div className="detail-info__title">Rain</div>
-          <div className="detail-info__options">4%</div>
+          <div className="detail-info__title">Precipitation</div>
+          <div className="detail-info__options">
+            {currentWeather.precipitation.total} mm
+          </div>
         </div>
         <div className="detail-info__box">
           <div className="detail-info__icon-box">
@@ -30,8 +51,10 @@ export const WeatherDetails = () => {
               size="20"
             />
           </div>
-          <div className="detail-info__title">Humidity</div>
-          <div className="detail-info__options">64%</div>
+          <div className="detail-info__title">Cloud cover</div>
+          <div className="detail-info__options">
+            {currentWeather.cloud_cover} %
+          </div>
         </div>
         <div className="detail-info__box">
           <div className="detail-info__icon-box">
@@ -39,7 +62,9 @@ export const WeatherDetails = () => {
           </div>
 
           <div className="detail-info__title">Wind</div>
-          <div className="detail-info__options">4 km/h</div>
+          <div className="detail-info__options">
+            {Math.round((currentWeather.wind.speed * 3600) / 1000)} km/h
+          </div>
         </div>
         <div className="detail-info__box">
           <div className="detail-info__icon-box">
