ab31edd54f675fa17921673d8a51c8e0ee3be899
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-09-13 15:53:08 +0200

    new logic weather day

--- src/entities/weather/UI/weather-day/WeatherDay.jsx
+++ src/entities/weather/UI/weather-day/WeatherDay.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { AButton, AIcon } from '@/shared/components/UI/atoms';
+import { AButton, AIcon, ALoader } from '@/shared/components/UI/atoms';
 import { selectWeather } from '@/redux/weather/weather-selectors.js';
 import { MSliderDay } from '@/shared/components/UI/molecules/MSliderDay';
 import { useConvertDegrees } from '../../hooks';
@@ -10,41 +10,43 @@ export const WeatherDay = () => {
   const allWeather = useSelector(selectWeather);
   const tempNow = useConvertDegrees(allWeather?.current?.temp);
 
-  const rainNow = allWeather?.hourly[0]?.pop;
-
-  const humidityNow = allWeather?.current?.humidity;
-  const windNow = allWeather?.current?.wind_speed;
-
-  return (
-    <div className="weather-day">
-      <div className="weather-day__container">
-        <div className="weather-day__icon">
-          <AIcon
-            name={allWeather?.current?.weather[0]?.description}
-            size="55"
-          />
-        </div>
-
-        <div className="weather-day__temperature-now">
-          {String(tempNow)?.slice(0, 2)}&#xb0;
-        </div>
-        <div className="weather-day__table">
-          <div className="weather-day__info">
-            <div className="weather-day__info-title">Rain</div>
-            <div className="weather-day__info-title">Humidity</div>
-            <div className="weather-day__info-title">Wind</div>
+  if (allWeather && tempNow) {
+    return (
+      <div className="weather-day">
+        <div className="weather-day__container">
+          <div className="weather-day__icon">
+            <AIcon
+              name={allWeather?.current?.weather[0]?.description}
+              size="55"
+            />
           </div>
-          <div className="weather-day__info">
-            <div className="weather-day__info-subtitle">{rainNow * 100} %</div>
-            <div className="weather-day__info-subtitle">{humidityNow} %</div>
-            <div className="weather-day__info-subtitle">
-              {Math.round(windNow)} km/h
+
+          <div className="weather-day__temperature-now">
+            {String(tempNow).slice(0, 2)}&#xb0;
+          </div>
+          <div className="weather-day__table">
+            <div className="weather-day__info">
+              <div className="weather-day__info-title">Rain</div>
+              <div className="weather-day__info-title">Humidity</div>
+              <div className="weather-day__info-title">Wind</div>
+            </div>
+            <div className="weather-day__info">
+              <div className="weather-day__info-subtitle">
+                {allWeather.hourly[0].pop * 100} %
+              </div>
+              <div className="weather-day__info-subtitle">
+                {allWeather.current.humidity} %
+              </div>
+              <div className="weather-day__info-subtitle">
+                {Math.round(allWeather.current.wind_speed)} km/h
+              </div>
             </div>
           </div>
         </div>
+        <MSliderDay />
+        <AButton to="/day">See More</AButton>
       </div>
-      <MSliderDay />
-      <AButton to="/day">See More</AButton>
-    </div>
-  );
+    );
+  }
+  return <ALoader />;
 };
